42d5fc97de0e6a4c7cffbf3a89eb9fce5c89390b
Karl Zipser <karlzipser@berkeley.edu>
2020-07-28 16:55:18 -0700

    gacp

--- utils3.py
+++ utils3.py
@@ -7,10 +7,11 @@ from k3.utils.connect import *
 
 from k3.utils.more import *
 
-log_name = get_safe_name(sys.argv[0].replace(opjk(),'')+'.txt')
-log_strs = []
-log_strs.append(' '.join(sys.argv).replace(opjk(),'')+' # '+time_str('Pretty'))
-os.system('mkdir -p '+opjk('__local__','logs'))
-list_of_strings_to_txt_file(opjk('__local__','logs',log_name),log_strs,write_mode="a")
+if False:
+	log_name = get_safe_name(sys.argv[0].replace(opjk(),'')+'.txt')
+	log_strs = []
+	log_strs.append(' '.join(sys.argv).replace(opjk(),'')+' # '+time_str('Pretty'))
+	os.system('mkdir -p '+opjk('__local__','logs'))
+	list_of_strings_to_txt_file(opjk('__local__','logs',log_name),log_strs,write_mode="a")
 
 #EOF
