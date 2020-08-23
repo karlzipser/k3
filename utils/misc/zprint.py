5342987ab8ba097143d35733295181891b2859ae
Karl Zipser <karlzipser@berkeley.edu>
2020-08-22 21:46:16 -0700

    gacp

--- utils/misc/zprint.py
+++ utils/misc/zprint.py
@@ -1,5 +1,17 @@
 
 from k3.utils.misc.printing import *
+import copy
+
+# https://pythonadventures.wordpress.com/2014/03/20/unicode-box-drawing-characters/
+
+def format_j(j):
+    assert(str_is_int(j))
+    s = str(j)
+    if len(s) == 1:
+        s = s+' '
+
+    s = cf(s,'`--d',' ')
+    return s
 
 def zprint(
     item,
@@ -13,24 +25,31 @@ def zprint(
     max_items=999999,
     r=0,
     p=0,
+    j=0,
+    K={},
 ):
 
-    title = t
+    name = t
+
+    kl=[]
 
     item_printed = False
 
     if type(item) in ignore_types:
         return
 
-    if type(title) not in [str,type(None)]:
-        title = str(title)
+    if type(name) not in [str,type(None)]:
+        name = str(name)
 
-    lst = []
+    lst = ['']
 
-    for i in range(len(space_increment)):
-        lst.append('.')
+    for i in range(len(space_increment)-1):
+        lst.append('─')
 
-    lst.append('.')
+    lst.append('┐')
+
+    if name == None:
+        lst[0] = '└'
 
     indent_text = ''.join(lst)
 
@@ -40,32 +59,52 @@ def zprint(
         if type(item) in [dict,list]:
             n_equals = cf(' (n=',len(item),')','`w-d',s0='',s1='')
 
-    if title != None:
-
-        if len(title) > len(indent_text):
-            indent_title = title
+    if name != None:
+        
+        if len(name) > len(indent_text):
+            indent_name = name
         else:
-            indent_title = title + indent_text[len(title):]
+            indent_name = name + indent_text[len(name):]
 
         if type(item) in [dict,list]:
-            clp(spaces,'`',indent_title,'`',n_equals,s0='',s1='')
+            clp(spaces,'`',indent_name,'`',n_equals,format_j(j),s0='',s1='')
+            j += 1
         else:
-            clp(spaces,'`',title,'','`y',' ','`',item,'`g',s1='',s0='' )
+            clp(spaces,'`',name,'','`','──','`',item,'`g',' ',format_j(j),s1='',s0='' )
             item_printed = True
-
+            j += 1
+        
     else:
         if type(item) in [dict,list]:
-            clp(spaces,indent_text,n_equals,s0='',s1='')
+            clp(spaces,indent_text,n_equals,format_j(j),s0='',s1='')
+            j += 1
 
-
+    kl__ = []
     if type(item) == list:
         ctr = 0
         for i in item:
-            zprint(i,t=None,spaces=spaces+space_increment,space_increment=space_increment,ignore_keys=ignore_keys,only_keys=only_keys,ignore_types=ignore_types,numbering=numbering)
+            j,kl_ = zprint(
+                i,
+                t=None,
+                spaces=spaces+space_increment,
+                space_increment=space_increment,
+                ignore_keys=ignore_keys,
+                only_keys=only_keys,
+                ignore_types=ignore_types,
+                numbering=numbering,
+                j=j,
+                K=K,
+            )
+            kl__ += kl_
             ctr += 1
             if ctr >= max_items:
                 break
+        kl.append(kl__)
+        cr(j in K)
+        K[j] = copy.deepcopy(kl)
+        cb(j)
     elif type(item) == dict:
+        
         ctr = 0
         for k in sorted(item.keys()):
             if k in ignore_keys:
@@ -77,12 +116,32 @@ def zprint(
                 l = len(item[k])
             else:
                 l = 1
-            zprint(item[k],t=k,spaces=spaces+space_increment,space_increment=space_increment,ignore_keys=ignore_keys,only_keys=only_keys,ignore_types=ignore_types,numbering=numbering)
+            j,kl_ = zprint(
+                item[k],
+                t=k,
+                spaces=spaces+space_increment,
+                space_increment=space_increment,
+                ignore_keys=ignore_keys,
+                only_keys=only_keys,
+                ignore_types=ignore_types,
+                numbering=numbering,
+                j=j,
+                K=K,
+            )
+            kl__ += kl_
+
             ctr += 1
             if ctr >= max_items:
-                break            
+                break
+        kl.append(kl__)
+        cm(j in K)  
+        K[j] = copy.deepcopy(kl)
+        cy(j)
+
     elif not item_printed:
-        color_print(spaces,item,'`g',s0='',s1='')
+        cf(spaces,item,'`g',s0='',s1='')
+
+    
 
     if p:
         time.sleep(p)
@@ -90,19 +149,40 @@ def zprint(
     if r:
         raw_enter()
 
+    if name is not None:
+        kl.insert(0,name)
+    
+    return j,kl
+
 
 if __name__ == '__main__':
     Q = {
-        '1':{
-            '2':{
-                '.meta':[5,6],
-                'a':1,
-            '7':{'xx':['a','v','r',[1,2,3]]},
-            '8':'eight',
-            }
+        'A':{
+            'B':[
+                    {'G':'g','H':'h',},
+                    {'E':'e','F':'f',},
+                ],
+            'I':'i',
         },
-        'qqq':'zzz',
+        'C':'c',
     }
-    zprint(Q)
-    
+    Q_ = {
+        'B':{
+            'E':'e',
+            'F':'f',
+        }
+    }
+    K = {}
+    j,kl = zprint(Q,t='TOP',K=K)
+    #kprint(kl,'kl')
+    #pprint(kl)
+    #kprint(K,'K')
+    K_ = {}
+    cmax = max(K.keys())
+    for k in K:
+        K_[cmax-k] = K[k]
+    del K
+    K = K_
+    kprint(K,'K',numbering=False)
+
 #EOF
