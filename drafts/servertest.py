ca52a2585c35533a699aea416715be0121fd8814
Karl Zipser <karlzipser@berkeley.edu>
2020-08-27 18:58:25 -0700

    gacp

---
+++ drafts/servertest.py
@@ -0,0 +1,47 @@
+from k3 import *
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+hostPort = 9000
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>", "utf-8"))
+        self.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
+        self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes(time_str('Pretty2'), "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+
+    def do_POST(self):
+        '''Reads post request body'''
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        content_len = int(self.headers.getheader('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        self.wfile.write("received post request:<br>{}".format(post_body))
+
+
+myServer = HTTPServer((hostName, hostPort), MyServer)
+print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
+
+try:
+    myServer.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+myServer.server_close()
+print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
+
+
+
+
+
+# https://riptutorial.com/python/example/26748/basic-handling-of-get--post--put-using-basehttprequesthandler
+
