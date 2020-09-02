715414ef355979a164a8e19c857d4fbbbf537194
Karl Zipser <karlzipser@berkeley.edu>
2020-09-02 14:54:18 -0700

    gacp

--- drafts/servertest.py
+++ drafts/servertest.py
@@ -8,14 +8,39 @@ hostPort = 9000
 class MyServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        if self.path.endswith('.jpeg'):
+            path_to_image = opjh("img.jpeg")
+            statinfo = os.stat(path_to_image)
+            img_size = statinfo.st_size
+            self.send_response(200)
+            self.send_header("Content-type", "image/jpg")
+            self.send_header("Content-length", img_size)
+            self.end_headers()
+            f = open(path_to_image, 'rb')
+            self.wfile.write(f.read())
+            f.close()  
+        else:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>", "utf-8"))
+            self.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
+            self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
+            self.wfile.write(bytes(time_str('Pretty2'), "utf-8"))
+            self.wfile.write(bytes("""<br><img src="/img.jpeg">""", "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+
+    def _do_GET(self):
+        path_to_image = opjh("img.jpeg")
+        statinfo = os.stat(path_to_image)
+        img_size = statinfo.st_size
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "image/jpg")
+        self.send_header("Content-length", img_size)
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
-        self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes(time_str('Pretty2'), "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        f = open(path_to_image, 'rb')
+        self.wfile.write(f.read())
+        f.close()   
 
 
     def do_POST(self):
