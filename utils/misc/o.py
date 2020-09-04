471b209dcb7edb2b04d5e770c4dcab1c21fbd363
Karl Zipser <karlzipser@berkeley.edu>
2020-09-04 09:44:36 -0700

    gacp

--- utils/misc/o.py
+++ utils/misc/o.py
@@ -3,11 +3,6 @@
 from k3.utils.misc.zprint import *
 
 
-
-
-
-
-
 def at_least_1_None(*l):
     for q in l:
         if q is None:
@@ -49,8 +44,9 @@ def o(
     elif wD is not None:
         assert sD is None
         D = wD
-    else sD is None and wD is None:
+    elif sD is None and wD is None:
         D = Last_D[0]
+
     assert_as(D is not None,"D is not None")
 
     assert_as(has_form_of_path(p),d2n('has_form_of_path(',p,')'))
@@ -66,8 +62,8 @@ def o(
         for k in key_list[:-1]:
             if k not in D:
                 if create_missing_paths:
-                    if report_implicit_path_creation:
-                        print( d2s('creating',k), W )
+                    if report_path_creation:
+                        print( d2s('creating',k) )
                     D[k] = {}
             D = D[k]
         k = key_list[-1]
@@ -78,165 +74,15 @@ def o(
 
 
 
-def _get_path(p,w,s,W):
-    prefix = W['current_prefix_path']
-
-    assert_as( at_least_1_None(w,s), "at_least_1_None(w,s)")
-
-    if has_form_of_alias(w):
-        w = W['aliases'][w]
-
-    if has_form_of_path(w):
-        prefix = w
-
-    if has_form_of_alias(s):
-        s = W['aliases'][s]
-
-    if has_form_of_path(s):
-        W['current_prefix_path'] = s
-        prefix = s
-
-    if has_form_of_alias(p):
-        assert_as(s is None, "s is None")
-        assert_as(w is None, "w is None")
-        path = W['aliases'][p]
-
-    elif has_form_of_path(p):
-        path = prefix + p
-
-    elif p is None:
-        path = prefix
-
-    else:
-        cE(p,'is neither path nor alias')
-
-    return path
-
-
-def _message(message,W):
-    W['messages'] = W['messages'][-W['max_num_messages']:]
-    W['message_ctr'] += 1
-    W['messages'].append((W['message_ctr'],message))
-    
-
-
-def _up_or_down(u,d,W):
-    
-    assert_as( at_least_1_None(u,d), "at_least_1_None(u,d)")
-
-    if u == 1:
-        if W['current_prefix_path'] == '~/':
-            _message("already at top",W)
-            return o() ###################
-        W['current_prefix_path'] = pname_(
-            W['current_prefix_path']
-        ) + '/'
-        _message('went up to '+W['current_prefix_path'],W)
-        return o( w=W['current_prefix_path'] ) ###################
-
-    if d == 1 or d == True or has_form_of_alias(d):
-        key_list = W['current_prefix_path'][:-1].split('/')
-        D = W['dictionary']
-        for k in key_list:
-            k = str_to_tuple_as_necessary(k)
-            D = D[k]
-        if type(D) == dict:
-            if has_form_of_alias(d):
-                if d in kys(D):
-                    W['current_prefix_path'] += d + '/'
-                    _message(d2s('down to',d),W)
-                    return o() ###################
-                else:
-                    assert False
-            if len(kys(D)) > 1:
-                k = select_from_list(kys(D))
-            else:
-                k = kys(D)[0]
-            W['current_prefix_path'] += k + '/'
-            _message(d2s('down to',k),W)
-        else:
-            _message("can't go down",W)
-        return o() ###################
-    elif d is None:
-        pass
-
-    else:
-        assert False
-
-
-
-
-
-def str_to_tuple_as_necessary(s):
-    if type(s) == str:
-        if len(s) > 1:
-            if s[-1] == ',':
-                if str_is_int(s[:-1]):
-                    return (int(s[:-1]),)
-    return s
-
-
-def pname_(path):
-    assert has_form_of_path(path)
-    path = path[:-1]
-    return pname(path)
-
-
-def input_int(s='> '):
-    c = input(s)
-    if str_is_int(c):
-        return int(c)
-    else:
-        return None
-
-
-def input_int_in_range(a,b,s):
-    c = input_int(s)
-    if c is None or c < a or c > b:
-        return None
-    else:
-        return c
-
-
-def select_from_list(lst):
-    for i in rlen(lst):
-        clp('    ',i,') ',lst[i],s0='')
-    i = input_int_in_range(0,len(lst)-1,'>> ')
-    return lst[i]
-
-
-
-
-
-
-
 if __name__ == '__main__':
 
-    if '__file__' in locals(): eg(__file__)
+    o('a/b/c/d/e/',e=1,sD={})
+    Q = o('a/b/c/e/')
+    o('a/b/c/e/',e=o('a/',wD=Q))#a/b/c/d/'))
+    #o('a/b/c/e/f/',2)
 
-    print(o('a/b/c/',e=1))
-
-    print(o('a/b/d/',e=456,a='d'))
-
-    zprint(o(),t='3')
-    
-    print(o('a/b/e/',e=789,a='e'))
-
-    zprint(o(),t='4')
-
-    print(o('a/f/g/h/',e=3,a='h'))
-
-    zprint(o(),t='5')
-
-    #print(o(s='~/a/f/'))
-
-    
-    #print(o('g/h/',e=6))
-
-    #zprint(o(),t='6')
-    
-    zprint(World)
-
+    zprint(Last_D[0])
+#,b
 
 if False:
     exec(gcsp()) ###############################################
