0d268f529d3bd098c4a952dd831db1d4cfe5eeb8
Karl Zipser <karlzipser@berkeley.edu>
2020-09-14 09:02:53 -0700

    gacp

--- misc/face_process_image.py
+++ misc/face_process_image.py
@@ -88,7 +88,7 @@ if boxes is not None and landmarks is not None:
             mci(a,title=d2s('resized',ctr))
             #cg(shape(a),r=1)
             #a = a.transpose(2,0,1)
-            a = a.transpose(2,0,1)
+            a = a.transpose(2,1,0)
             #cg(shape(a),r=1)
             b = torch.from_numpy(a).float()
             #cb(b.size(),r=1)
@@ -135,52 +135,16 @@ if False:
         spause()
 
     mi(o[0]['face'],100)
+    figure(2);clf();plt_square()
     for i in rlen(o):
-        figure(2);clf();plt_square()
+        figure(2)#;clf();plt_square()
         plot(o[0]['embedding'],o[i]['embedding'],'.')
         mi(o[i]['face'],101)
         spause()
 
 
 
-def ___Facenet():
 
-    from facenet_pytorch import MTCNN
-    import torch
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    mtcnn = MTCNN(keep_all=True, device=device)
-
-    def get_boxes(frame):
-        boxes, probs, landmarks = mtcnn.detect(frame, landmarks=True)
-        #boxes, _ = mtcnn.detect(frame)
-        return boxes, probs, landmarks
-
-    def draw_boxes(frame,boxes,landmarks):
-        frame_with_boxes = frame.copy()
-        for box, landmark in zip(boxes, landmarks):
-            x0,x1,y0,y1 = intr(box[0]),intr(box[2]),intr(box[1]),intr(box[3])
-            dx = x1 - x0
-            dy = y1 - y0
-            if True:
-                cv2.line(frame_with_boxes,(x0,y0),(x1,y0),(255,0,0),4)
-                cv2.line(frame_with_boxes,(x0,y1),(x1,y1),(255,0,0),4)
-                cv2.line(frame_with_boxes,(x0,y0),(x0,y1),(255,0,0),4)
-                cv2.line(frame_with_boxes,(x1,y0),(x1,y1),(255,0,0),4)
-        return frame_with_boxes
-
-    return namedtuple(
-        '_',
-        'get_boxes draw_boxes')(
-         get_boxes, draw_boxes
-    )
-
-if False: #e.g.
-    img = zimread('path')
-    F = Facenet()
-    a,b,c = F.get_boxes(img)   
-    d = F.draw_boxes(img,a,c) 
-    CA()
-    mi(d)
 #,b
 
 #EOF
