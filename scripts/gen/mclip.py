481a3c1a3bbe220d38b55a44bded32dcbe2d2dd8
Karl Zipser <karlzipser@berkeley.edu>
2020-11-24 08:13:49 -0800

    gacp

--- scripts/gen/mclip.py
+++ scripts/gen/mclip.py
@@ -33,10 +33,11 @@ E = {
 	'noisy':False,
 	'num_chars':100,
 	'lst':lst,
-	'pause_thread':False,
+	#'quit':False,
 }
 
 #istr =  cf('--> ','`gb-')
+
 
 def clipthread():
 
@@ -49,13 +50,9 @@ def clipthread():
 
 		if E['Command'] == 'quit':
 			cg('quitting clipthread()')
+
 			return 0
 
-		if E['pause_thread']:
-			E['thread_sleeping'] = True
-			time.sleep(0.2)
-			E['thread_sleeping'] = False
-			return 0
 
 		c = getClipboardData()
 
@@ -78,25 +75,12 @@ def clipthread():
 			E['lst'].append( [c,s] )
 
 			print_lst()
-			#print(istr)
 
 			so(E['lst'],save_file,noisy=E['noisy'])
 			E['noisy'] = False
 
 		
-def _print_lst():
-	clear_screen()
-	for i in rlen(E['lst']):
-		m = min(len(E['lst'][i][0]),E['num_chars'])
-		a = E['lst'][i][0][:m]
-		b = a.split('\n')
-		c = []
-		for d in b:
-			if len(d) > 0:
-				c.append(d)
-		clp(str(i)+')','`--b',E['lst'][i][1],'`--d','\n'+'\n'.join(c), '`-wb') #'`wbb')
-		if m < len(E['lst'][i][0]):
-			clp('...','`bwb')
+
 
 def print_lst():
 	rows,cols = get_terminal_size()
@@ -122,57 +106,16 @@ threading.Thread(target=clipthread).start()
 while True:
 
 	try:
-
-		q = raw_input()
-
-		r = q.split(' ')
-		if str_is_int(r[0]):
-			setClipboardData(E['lst'][int(r[0])][0])
-			clp('set clipboard to:\n','`g',qtd(E['lst'][int(r[0])][0]),'`m')
-		elif r[0] == 'q':
-			E['Command'] = 'quit'
-			break
-		elif r[0] == 'r':
-			u = 0
-			if len(r) == 3:
-				u  = int(r[2]) + 1
-			elif len(r) == 2:
-				u = int(r[1])+1
-			if u:
-				for v in range(int(r[1]),u):
-					E['lst'].pop(int(r[1]))
-				so(E['lst'],save_file,noisy=E['noisy'])
-				print_lst()
-		elif r[0] == 'n':
-			if int(r[1]) < 0:
-				E['num_chars'] = 999999
-			else:
-				E['num_chars'] = int(r[1])
-			print_lst()
-		elif r[0] == 'l':
-			files = sggo(save_dir,'*.pkl')
-			for i in rlen(files):
-				if '.pkl' in files[i]:
-					clp(i,')',fname(files[i]))
-			j = input('==>> ')
-			assert(str_is_int(j))
-			j = int(j)
-			print(files[j])
-			E['lst'] = lo(files[j])
-			print_lst()
-		elif r[0] == 'x':
-			if use_prefix_and_suffix:
-				use_prefix_and_suffix = False
-			else:
-				use_prefix_and_suffix = True
-
-		elif r[0] == 'p':
-			E['pause_thread'] = not E['pause_thread']
+		while True:
+			time.sleep(1)
+			
+			if E['Command'] == 'quit':
+				sys.exit()
 
 	except KeyboardInterrupt:
 	    print('*** KeyboardInterrupt ***')
 	    E['Command'] = 'quit'
-	    time.sleep(0)
+	    time.sleep(1)
 	    sys.exit()
 	except Exception as e:
 	    exc_type, exc_obj, exc_tb = sys.exc_info()
