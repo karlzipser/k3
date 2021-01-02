f82b5d797f32cc2b1a340a7aeda8007bcabaa283
Karl Zipser <karlzipser@berkeley.edu>
2021-01-02 12:40:23 -0500

    gacp

--- scripts/gen/time_lapse_cam.py
+++ scripts/gen/time_lapse_cam.py
@@ -3,107 +3,140 @@ from k3.utils import *
 #,tcam.a
 """
 python3 k3/scripts/gen/time_lapse_cam.py\
-    -t .1\
-    -s\
-    -f False\
-    -m 30.\
-    -d 117062205.\
-
-
+    --mint .1\
+    --show\
+    --long 30.\
+    --diff 117062205.\
+    --desired 10\
 """
 #,tcam.b
 
 
-# long interval, max num shots in long interval, short interval, total duration
 A = get_Arguments(
     {
-        ('t','sleep time (s)'):10.,
-        ('s','show'):True,
-        ('f','flip left right'):False,
-        ('p','path'):opjh('scratch'),
-        ('d','image difference threshold'):190000000.,
-        ('m','max sleep time(s)'):60.,
-        ('r','record for real'):False,
-        ('g','show graph'):False,
-        ('b','beep'):False,
+        ('mint','min timestep (s)'):10.,
+        ('show','show'):True,
+        ('flip','flip left right'):False,
+        ('path','path'):opjh('scratch'),
+        ('diff','image difference diff_'):190000000.,
+        ('long','longer interval (s)'):60.,
+        ('desired','desired # shots in long interval'):5,
+        ('record','record for real'):False,
+        ('graph','show graph'):False,
+        ('beep','beep'):False,
     }
 )
-sleeptime = A['t']
-show_frame = A['s']
-record_for_real = A['r']
-threshold = A['d']
+
+for k in kys(A):
+    if k[0] != '-':
+        s = A[k]
+        if type(s) is str:
+            s = qtd(s)
+        else:
+            s = str(s)
+        exec(k+'_ = '+s)
+
+print(mint_,show_,flip_,path_,diff_,long_,desired_,record_,graph_,beep_)
+
 
 video_capture = cv2.VideoCapture(0)
+
 d = datetime.date.today()
+
+long_timer = Timer(long_)
+
 path = opj(
-    A['p'],
+    path_,
     str(d.year),
     str(d.today().month),
     str(d.today().day),
     'timelapse.'+time_str()
 )
-print(path)
-os_system('mkdir -p',path)
+
+os_system('mkdir -p',path,e=1)
 
 last_time = 0
 
 img_dif = []
+
+shot_times = []
+
 frame,prev_frame = False,False
-max_sleep = Timer(A['m'])
+
 
 while True:
 
-    if time.time()-last_time > sleeptime:
+    if time.time()-last_time > mint_:
+
         if type(frame) is not bool:
             prev_frame = frame
+
         ret, frame = video_capture.read()
 
         if type(prev_frame) is not bool:
+
             img_dif.append( 
-                sum(sum(sum(( prev_frame.astype(float) - frame.astype(float) )**2))))
+                sum(sum(sum(
+                    (prev_frame.astype(float) - frame.astype(float) )**2
+                )))
+            )
             
-            if A['g']:
-                figure(1)
+            if graph_:
+                figure(1,figsize=(10,2))
                 clf()
-                plot(img_dif)
-                plot([0,len(img_dif)],[threshold,threshold],'r')
-                ylim([0,5*threshold])
+                plot(img_dif,'k')
+                plot([0,len(img_dif)],[diff_,diff_],'r')
+                
+                ylim([0,5*diff_])
                 spause()
             else:
                 plt.close('all')
-            #print(img_dif[-1])
 
-
-        if len(img_dif) >= 10:
-            if img_dif[-1] > threshold:  #2 * np.std(na(img_dif)):# [-10:])):
-                record = True
-            else:
-                record = False
-        else:
+        if len(img_dif) > 0 and img_dif[-1] > diff_:
             record = True
+        else:
+            record = False
 
-        if max_sleep.check():
-            max_sleep.reset()
+        if long_timer.check():
+            long_timer.reset()
             record = True
 
         last_time = time.time()
 
         if record:
+
+            shot_times.append(time.time())
+
             fname_ = opj(path,d2p(dp(time.time()),'jpg'))
 
-            if record_for_real:
+            if record_:
                 q = '* '
             else:
                 q = '- '
             print(q+fname(fname_))
-            if A['b']:
+            if beep_:
                 beep()
-            if record_for_real:
+            if record_:
                 imsave(fname_,frame)#rgbframe)
 
-        if show_frame:
+
+        n = 0
+        t = time.time()
+        for i in range(-1,-len(shot_times),-1):
+            if t - shot_times[i] > long_:
+                break
+            n += 1
+        if n > desired_:
+            diff_ *= 1.01
+            cg('up')
+        elif n < desired_:
+            diff_ *= (1/1.01)
+            cr('down')
+        print(n,int(diff_))
+
+        if show_:
             if record:
-                if record_for_real:
+                if record_:
                     c = [0,0,255]
                 else:
                     c = [0,255,0]
@@ -120,19 +153,19 @@ while True:
         break
 
     elif k & 0xFF == ord('r'):
-        record_for_real = not record_for_real
+        record_ = not record_
 
     elif k & 0xFF == ord('u'):
-        threshold *= 1.1
-        print('threshold',dp(threshold))
+        diff_ *= 1.1
+        print('diff_',dp(diff_))
 
     elif k & 0xFF == ord('d'):
-        threshold *= (1/1.1)
-        print('threshold',dp(threshold))
+        diff_ *= (1/1.1)
+        print('diff_',dp(diff_))
 
     elif k & 0xFF == ord('g'):
-        print(A['g'])
-        A['g'] = not A['g']
+        print(graph_)
+        graph_ = not graph_
 
 # When everything is done, release the capture
 video_capture.release()
